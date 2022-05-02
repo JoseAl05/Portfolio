8ccb858efc83cbc9209636f712a87a16b25d915e
JoseAl05 <jparancibialinker@gmail.com>
2022-05-02 14:13:20 -0400

    Added more Tools

--- src/components/Tools/Tools.jsx
+++ src/components/Tools/Tools.jsx
@@ -1,9 +1,8 @@
 import React,{useState} from "react";
 import Grid from '@mui/material/Grid';
 import { useTransition,useSpringRef,useChain,useSpring,config,animated } from "react-spring";
-import express from "../../images/express.png";
-import {FaReact,FaNodeJs,FaAngular,FaArrowUp,FaArrowDown} from 'react-icons/fa';
-import {SiJavascript,SiMysql,SiPostgresql,SiSequelize,SiMicrosoftsqlserver} from 'react-icons/si';
+import {FaReact,FaNodeJs,FaAngular,FaArrowDown} from 'react-icons/fa';
+import {SiJavascript,SiMysql,SiPostgresql,SiSequelize,SiMicrosoftsqlserver,SiMongodb,SiCss3,SiHtml5,SiExpress} from 'react-icons/si';
 import "./Tools.css";
 
 
@@ -11,11 +10,6 @@ const Tools = () =>{
 
     const [isVisibleTools,setIsVisibleTools] = useState(false);
 
-    // const transitionTools = useTransition(isVisibleTools,{
-    //     from:{x: 0, y: 100, opacity: 0},
-    //     enter:{x: 0, y: -10, opacity: 1},
-    //     leave:{x: 0, y: 100, opacity: 0},
-    // })
     const springApi = useSpringRef()
     const { size, ...rest } = useSpring({
       ref: springApi,
@@ -56,12 +50,15 @@ const Tools = () =>{
                                         <animated.span style={{...style}} className="icon-name"><FaReact color="#61dafb" className="icon" size={60}/><br/>ReactJs</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><FaAngular color="#DD0031" className="icon" size={60}/><br/>Angular</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><SiJavascript color="yellow" className="icon" size={60}/><br/>JavaScript</animated.span>
+                                        <animated.span style={{...style}} className="icon-name"><SiHtml5 color="#e34f26 " className="icon" size={60}/><br/>HTML</animated.span>
+                                        <animated.span style={{...style}} className="icon-name"><SiCss3 color="#264de4 " className="icon" size={60}/><br/>CSS</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><FaNodeJs color="#84ba64" className="icon" size={60}/><br/>NodeJS</animated.span>
-                                        <animated.span style={{...style}} className="icon-name"><img src={express} className="icon" alt="express"/><br/>ExpressJS</animated.span>
+                                        <animated.span style={{...style}} className="icon-name"><SiExpress color="#000000" className="icon" size={60}/><br/>ExpressJS</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><SiSequelize color="#399af3" className="icon" size={60}/><br/>Sequelize</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><SiMysql color="#3E6E93" className="icon" size={60}/><br/>MySQL</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><SiPostgresql color="#336791" className="icon" size={60}/><br/>PostgreSql</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><SiMicrosoftsqlserver className="icon" color="red" size={60}/><br/>SQL Server</animated.span>
+                                        <animated.span style={{...style}} className="icon-name"><SiMongodb color="#589636" className="icon" size={60}/><br/>MongoDB</animated.span>
                                     </div> : ''
                             )}
                         </div>
