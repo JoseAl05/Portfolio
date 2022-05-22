99b7804e8775fb49f4d2ffbbff7a2898924dd96f
JoseAl05 <jparancibialinker@gmail.com>
2022-05-22 14:17:26 -0400

    Update tools

--- src/components/Tools/Tools.jsx
+++ src/components/Tools/Tools.jsx
@@ -2,7 +2,7 @@ import React,{useState} from "react";
 import Grid from '@mui/material/Grid';
 import { useTransition,useSpringRef,useChain,useSpring,config,animated } from "react-spring";
 import {FaReact,FaNodeJs,FaAngular,FaArrowDown} from 'react-icons/fa';
-import {SiJavascript,SiMysql,SiPostgresql,SiSequelize,SiMicrosoftsqlserver,SiMongodb,SiCss3,SiHtml5,SiExpress} from 'react-icons/si';
+import {SiJavascript,SiMysql,SiPostgresql,SiSequelize,SiMicrosoftsqlserver,SiMongodb,SiCss3,SiHtml5,SiExpress,SiNextdotjs} from 'react-icons/si';
 import "./Tools.css";
 
 
@@ -47,7 +47,8 @@ const Tools = () =>{
                             {transitionIcons((style,item) =>
                                 item ?
                                     <div className="tools-icons">
-                                        <animated.span style={{...style}} className="icon-name"><FaReact color="#61dafb" className="icon" size={60}/><br/>ReactJs</animated.span>
+                                        <animated.span style={{...style}} className="icon-name"><FaReact color="#61dafb" className="icon" size={60}/><br/>ReactJS</animated.span>
+                                        <animated.span style={{...style}} className="icon-name"><SiNextdotjs color="#000000" className="icon" size={60}/><br/>NextJS</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><FaAngular color="#DD0031" className="icon" size={60}/><br/>Angular</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><SiJavascript color="yellow" className="icon" size={60}/><br/>JavaScript</animated.span>
                                         <animated.span style={{...style}} className="icon-name"><SiHtml5 color="#e34f26 " className="icon" size={60}/><br/>HTML</animated.span>
